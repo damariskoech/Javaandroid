01742e099bc3a538d2f282cb4f40edfea3437a3c
damaris koech <61495438+damariskoech@users.noreply.github.com>
2020-03-04 20:57:55 +0300

    imported a utility scanner that enables the user to input two integers
    Signed-off-by: damaris koech <61495438+damariskoech@users.noreply.github.com>

--- app/src/main/java/uservariables.java
+++ app/src/main/java/uservariables.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 {
     public static void main(String args[])
     {
-        int p, q, r;
+        int num1, num2, sum;
         System.out.println("Enter two integers to calculate their sum: ");
         Scanner sc = new Scanner(System.in); //System.in An InputStream which is typically connected to keyboard input of console programs.
-        p = sc.nextInt(); //scan data for input of integer value
-        q = sc.nextInt();
-        r = p + q;
-        System.out.println("Sum of entered integers = "+r);
+        num1 = sc.nextInt(); //scan data for input of integer value
+        num2 = sc.nextInt();
+        sum = num1 + num2;
+        System.out.println("Sum of entered integers = "+sum);
     }
 }
