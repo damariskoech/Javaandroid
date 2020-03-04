9eeb48c8b441287f0c82579202507a45f94b44a6
damaris koech <61495438+damariskoech@users.noreply.github.com>
2020-03-04 14:08:49 +0300

    added multiple line block comment

--- app/src/main/java/Helloworld.java
+++ app/src/main/java/Helloworld.java
@@ -1,11 +1,12 @@
-/* This is a java example that demonstrates printing statements in multiple lines*/
+/* This is a java example that demonstrates use of printing statements in multiple lines*/
 
 public class Helloworld {
-    public static void main(String args[]) {
+    public static void main(String [] args) {
         //This prints statements in multiple lines
         System.out.println("Java");
         System.out.println("Android");
         System.out.println("Test");
+
     }
 }
 
