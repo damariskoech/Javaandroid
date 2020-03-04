9be3011d2f06e23b6903d0e5d8b65860d31a8b19
damaris koech <61495438+damariskoech@users.noreply.github.com>
2020-03-04 14:33:20 +0300

    added multiple line block comment
    Signed-off-by: damaris koech <61495438+damariskoech@users.noreply.github.com>

---
+++ app/src/main/java/Additionvariables.java
@@ -0,0 +1,15 @@
+/* This is a java example that demonstrates addition of variables*/
+
+public class Additionvariables {
+    public static void main(String [] args) {
+
+        System.out.println("this is an addition program");
+        int x = 23;
+        int y = 45;
+        int z = x+y;
+        System.out.println(z);
+
+    }
+}
+
+
