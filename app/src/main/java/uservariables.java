cd174327a3eb2e2ef84d2b41699c42002bb95ac3
damaris koech <61495438+damariskoech@users.noreply.github.com>
2020-03-04 15:59:59 +0300

    added a scanner utility package for addition
    Signed-off-by: damaris koech <61495438+damariskoech@users.noreply.github.com>

--- app/src/main/java/uservariables.java
+++ app/src/main/java/uservariables.java
@@ -1,17 +1,14 @@
-public class uservariables{
-    public static void main(String [] args){
-        int num1,num2,sum;
-        scanner.sc = new scanner(System.in);
-        System.out.println("Enter the first number:");
-        num1 = sc.nextInt();
-
-        System.out.println("Enter the second number:");
-        num2 = sc.nextInt();
-
-        sc.close();
-        sum = num1 + num2;
-        System.out.println("the sum of these numbers;"+sum);
-
-
+import java.util.Scanner;
+ class uservariables
+{
+    public static void main(String args[])
+    {
+        int p, q, r;
+        System.out.println("Enter two integers to calculate their sum: ");
+        Scanner sc = new Scanner(System.in); //System.in An InputStream which is typically connected to keyboard input of console programs.
+        p = sc.nextInt(); //scan data for input of integer value
+        q = sc.nextInt();
+        r = p + q;
+        System.out.println("Sum of entered integers = "+r);
     }
 }
