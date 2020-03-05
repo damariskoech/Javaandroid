427c2ac0cb285cb24444453de187cac693f95d7d
damaris koech <61495438+damariskoech@users.noreply.github.com>
2020-03-05 12:30:21 +0300

    added assignment operators
    Signed-off-by: damaris koech <61495438+damariskoech@users.noreply.github.com>

---
+++ app/src/main/java/assignmentoperators.java
@@ -0,0 +1,15 @@
+public class assignmentoperators {
+
+    public static void main(String[] args) {
+        int num1=23,num2=32,sum;
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        num1 %= num2;
+        System.out.println("num1 = " + num1);
+
+
+
+    }
+
+}
