e1432cc3df51ed3fc6f02bc2478b08651b425887
damaris koech <61495438+damariskoech@users.noreply.github.com>
2020-03-06 11:45:48 +0300

    added the Ifelse_operators
    Signed-off-by: damaris koech <61495438+damariskoech@users.noreply.github.com>

--- app/src/main/java/IfElse_operators.java
+++ app/src/main/java/IfElse_operators.java
@@ -1,2 +1,12 @@
 public class IfElse_operators {
+    public static void main(String args[]) {
+        int Age = 18;
+        int DOB = 1998;
+        if (Age >= 18 && DOB >= 1998) {
+            System.out.print("He is Authorized");
+        } else {
+            System.out.print("He is NOT Authorized");
+        }
+    }
+
 }
